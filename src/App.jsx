d0ee29a38bd480bbb06fa55a86fd305a7821bc5d
Virsaviya Efraim <vefraim@meta.com>
2025-05-08 16:25:41 -0700

    Set api key to .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 const App = () => {
-  <div className="App">
-    
-  </div>
-}
+  return <div className='App'>Hi Flixter</div>;
+};
 
-export default App
+export default App;
